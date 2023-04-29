b1fe3fa7ade6683c89913d1a55a05c0ab8a50c96
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-04-29 16:27:24 +0200

    Exercise 6 done

--- esercizi_da_inviare/src/App.js
+++ esercizi_da_inviare/src/App.js
@@ -1,20 +1,12 @@
 
-const Welcome = ({name}) => {
+const Welcome = ({name= "Pietro", age = 25}) => {
     return(
-        <p>Welcome, {name}</p>
+        <>
+            <p>Welcome, {name}</p>
+            <p>Your age is {age}</p>
+        </>
     )
 } 
 
 export default Welcome
 
-//What happens if no name prop is passed to the Welcome component? 
-// It Will not export any value
-
-// Can you set a default value for the name prop?
-// 2. Yes, you can like that:
-
-// const Welcome = ({name = "Pietro"}) => {
-//     return(
-//         <p>Welcome, {name}</p>
-//     )
-// } 

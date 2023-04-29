455da0f5c1de6a4c2ee84cc6b47448378369c08f
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-04-29 16:39:41 +0200

    Exercise 8 done

---
+++ esercizi_da_inviare/src/Age.js
@@ -0,0 +1,8 @@
+
+const Age = ({age = 25}) => {
+    return (
+        <p>Your age is {age}</p>
+    )
+}
+
+export default Age
--- esercizi_da_inviare/src/App.js
+++ esercizi_da_inviare/src/App.js
@@ -1,14 +1,13 @@
+import Age from "./Age"
 
 const Welcome = ({name = <strong>Pietro</strong>, age = 25}) => {
     return(
         <>
             <p>Welcome, {name}</p>
-            <p>Your age is {age}</p>
+            <Age/>
         </>
     )
 } 
 
 export default Welcome
 
-// What happens if the name prop is a JSX expression instead of a string? 
-// It, in this case, will change the font of Pietro in bold

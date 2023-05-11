d058103cbd8c6117836a0b9e6503624bfecdce85
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-05-11 18:59:51 +0200

    dropping exercise 24

--- esercizi_da_inviare/src/App.js
+++ esercizi_da_inviare/src/App.js
@@ -1,11 +1,14 @@
 import InteractiveWelcome from "./InteractiveWelcome"
 import Login from "./Login"
 
+import UncontrolledLogin from "./UncontrolledLogin"
+
 const App = () =>{
     return(
         <>
             <InteractiveWelcome  />
             <Login />
+            <UncontrolledLogin />
             
         </>
     )
--- esercizi_da_inviare/src/UncontrolledLogin.js
+++ esercizi_da_inviare/src/UncontrolledLogin.js
@@ -1,6 +1,29 @@
 
+
 const UncontrolledLogin = () => {
-    
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault()
+        
+        const username = event.target.username.value = "";
+        const password = event.target.password.value = "";
+
+        
+        const ciao = true ? username === "" || password === "" : false
+        
+    }
+
+
+    return(
+        <>
+            <form onSubmit={handleFormSubmit}>
+                <input type="Text" name="username"></input>
+                <input type="password" name="password"></input>
+                <button name="login" type="submit" >Login</button>
+                <button>Reset</button>
+            </form>
+        </>
+    )
 }
 
 export default UncontrolledLogin

370febcda42afb05674fed33ecd71532f7eb76b5
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-05-19 18:14:20 +0200

    Exercise 23 done

--- esercizi_da_inviare/src/App.js
+++ esercizi_da_inviare/src/App.js
@@ -1,12 +1,13 @@
 import InteractiveWelcome from "./InteractiveWelcome"
 import Login from "./Login"
+import UncontrolledLogin from "./UncontrolledLogin"
 
 const App = () =>{
     return(
         <>
             <InteractiveWelcome  />
             <Login />
-            
+            <UncontrolledLogin />
         </>
     )
 }

862b398a3b670d2f43022c8d4f90b25c923803b3
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-05-19 18:11:10 +0200

    Exercise 24 done

---
+++ esercizi_da_inviare/src/UncontrolledComponent.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+class UncontrolledLogin extends React.Component {
+    handleLogin = (event) => {
+        event.preventDefault();
+        const username = event.target.elements.username.value
+        const password = event.target.elements.password.value
+        const remember = event.target.elements.remember.checked
+
+        console.log({
+            username,
+            password,
+            remember,
+        });
+    }
+
+
+    render() {
+        return (
+            <>
+                <form onSubmit={this.handleLogin}>
+                    <input name="username" autoFocus/>
+                    <input name="password" type="password"/>
+                    <input name="remember" type="checkbox"/>
+                    <button type="submit">Login</button>
+                    <button type="reset">Reset</button>
+                </form>
+            </>
+        )
+    }
+}
+
+export default UncontrolledLogin;

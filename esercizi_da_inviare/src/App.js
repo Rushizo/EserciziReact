102739701974cb6939f60c6f9d202e7451a81ec9
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-05-21 23:19:42 +0200

    Exercise 50 done

--- esercizi_da_inviare/src/App.js
+++ esercizi_da_inviare/src/App.js
@@ -1,20 +1,24 @@
+import GithubUserList from "./GithubUserList"
+import ShowGithubUser from "./ShowGithubUser"
 import { Routes } from "react-router-dom"
 import { Route } from "react-router-dom"
 import { Link } from "react-router-dom"
-import ShowGithubUser from "./ShowGithubUser"
 
 const App = () => {
 
     return(
         <>
         <Routes>
-            <Route path="/" element={<ShowGithubUser />} />
-            <Route path="users/:username" element={<ShowGithubUser />} />  
-            <Route path="*" element={<h1>Page not found!</h1>} />      
+           <Route path="users" >
+              <Route path="list" element={<GithubUserList />} >
+                <Route path=":username" element={<ShowGithubUser/>} />
+              </Route>
+           </Route>
         </Routes>
-        <p><Link to="users/rushizo">Link to my repository</Link></p>
-        <p><Link to="users/yangus98">Link to my friend's repository</Link></p>
-        <p><Link to="users/RobertoFanara">Link to my friend Roberto</Link></p>
+
+        <p><Link to="users/list/rushizo">Link to my repository</Link></p>
+        <p><Link to="/">Link to my Homepage</Link></p>
+        <p><Link to="users/list">Link to the list</Link></p>
         </>
     )
 }
---
+++ esercizi_da_inviare/src/GithubUserList.js
@@ -0,0 +1,24 @@
+import { Link, Outlet } from "react-router-dom";
+import {useState} from "react"
+
+const GithubUserList = () => {
+    const [name, setName] = useState('');
+    const [items, setItems] = useState([]);
+    
+    const handleAdd = () => {
+        setItems([...items, name]);
+    };
+
+    return(
+        <div>
+        <input onChange={(e) => setName(e.target.value)}/>
+          <button onClick={handleAdd}>add</button>
+          {items.map((item, index) => {return <li index={index}><Link to={item}>{item}</Link></li>})}
+
+          <Outlet />
+        </div>
+
+    )
+}
+
+export default GithubUserList

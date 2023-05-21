090c07f5b7b92f2c251162b567b7c89edad14b33
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-05-21 23:03:44 +0200

    Exercise 47 done

--- esercizi_da_inviare/src/App.js
+++ esercizi_da_inviare/src/App.js
@@ -1,13 +1,15 @@
 import { Routes } from "react-router-dom"
 import { Route } from "react-router-dom"
-import ClickCounter from "./ClickCounter"
+import ShowGithubUser from "./ShowGithubUser"
 
 const App = () => {
     return(
         <>
         <Routes>
-            <Route path="/" element={<ClickCounter />} />
+            <Route path="/" element={<ShowGithubUser />} />
         </Routes>
         </>
     )
 }
+
+export default App
